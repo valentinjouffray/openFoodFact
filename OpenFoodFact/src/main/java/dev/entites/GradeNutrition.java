5e173beebff9b1722085994a038e01c33012b19f
valentin <valentin.jouffray@gmail.com>
2023-04-19 17:22:45 +0200

    fix(gradeNutrition): ajout de la méthode getGrade

--- OpenFoodFact/src/main/java/dev/entites/GradeNutrition.java
+++ OpenFoodFact/src/main/java/dev/entites/GradeNutrition.java
@@ -1,5 +1,32 @@
 package dev.entites;
 
 public enum GradeNutrition {
-    A,B,C,D,E
+    A,B,C,D,E;
+    public String getGrade() {
+        switch (this){
+            case A -> {
+                return "A";
+            }
+            case B -> {
+                return "B";
+            }
+            case C -> {
+                return "C";
+            }
+            case D -> {
+                return "D";
+            }
+            case E -> {
+                return "E";
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("GradeNutrition{");
+        sb.append('}');
+        return sb.toString();
+    }
 }
