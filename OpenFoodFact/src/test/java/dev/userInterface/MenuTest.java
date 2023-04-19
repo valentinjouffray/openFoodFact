0a6faa1ee8fff7b5d47d8b2a6a598d7a4aae7908
valentin <valentin.jouffray@gmail.com>
2023-04-19 14:00:51 +0200

    feat(menu): menu ecrit

---
+++ OpenFoodFact/src/test/java/dev/userInterface/MenuTest.java
@@ -0,0 +1,11 @@
+package dev.userInterface;
+
+import org.junit.Test;
+
+public class MenuTest {
+    @Test
+    public void testAfficherMenuPrincipal() {
+        Menu menu = new Menu();
+        menu.afficherMenuPrincipal();
+    }
+}
